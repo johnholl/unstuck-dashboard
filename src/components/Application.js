f79cb1b53fbc11abec4e80054df92c8d11f9f523
John <johnholl@umich.edu>
2020-12-20 20:31:43 -0500

    fix login redirect and login styling type(scope): description
    body

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Spin } from 'antd';
 import { UserContext } from '../providers/UserProvider';
 import Dashboard from './dashboard/dashboard';
+import DashRedirect from './dashboard/dashRedirect';
 import bookingInfo from './booking/bookingInfo';
 import CreateService from './service/createService';
 import EditService from './service/editService';
@@ -18,6 +19,7 @@ export default function Application() {
   return user ? (
     <Router>
       <div className="App">
+      <Route path="/" component={DashRedirect} />
         <Route path="/bookingInfo" component={bookingInfo} />
         <Route path="/newService" component={CreateService} />
         <Route path="/editService" component={EditService} />
--- src/components/authentication/LoginScreen.js
+++ src/components/authentication/LoginScreen.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, Typography, Row } from 'antd';
 import { auth } from '../../firebase';
+
+const {Title} = Typography;
 
 const layout = {
   labelCol: {
     span: 8,
   },
   wrapperCol: {
-    span: 16,
+    span: 8,
   },
 };
 
@@ -30,7 +32,7 @@ export default function SignIn(props) {
     console.log('we signing in right here');
     auth
       .signInWithEmailAndPassword(values.userEmail, values.userPassword)
-      .then(() => props.history.push('/'))
+      .then(() => props.history.push('/dashboard'))
       .catch((error) => {
         setError('Error signing in with password and email!');
         console.error('Error signing in with password and email', error);
@@ -38,9 +40,10 @@ export default function SignIn(props) {
   };
 
   return (
-    <div className="mt-8">
-      <h1 className="text-3xl mb-2 text-center font-bold">Sign In</h1>
-      <div className="border border-blue-400 mx-auto w-11/12 md:w-2/4 rounded py-8 px-4 md:px-8">
+    <div style={{backgroundColor:"#f9f0ff", minHeight: 'calc(100vh - 10px)' }}>
+      <Row justify="center" style={{paddingTop:50}}>
+        <Title level={2}>Sign in</Title>
+      </Row>
         {error !== null && (
           <div className="py-4 bg-red-600 w-full text-white text-center mb-3">
             {error}
@@ -72,11 +75,10 @@ export default function SignIn(props) {
 
           <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
             <Button type="primary" htmlType="submit">
-              Submit
+              Sign in
             </Button>
           </Form.Item>
         </Form>
-      </div>
     </div>
   );
 }
