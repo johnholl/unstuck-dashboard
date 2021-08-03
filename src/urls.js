3038ccf8e185d76dfa3d5ecdac8bcc9456ac391c
John Holler <johnholl@umich.edu>
2021-08-02 21:22:31 -0400

    migrated to beunstuck.net type(scope): description
    body

--- src/urls.js
+++ src/urls.js
@@ -3,6 +3,6 @@
 export const astronautUrl = "https://firebasestorage.googleapis.com/v0/b/unstuck-backend.appspot.com/o/undraw_Astronaut_re_8c33.svg?alt=media&token=cef7909c-6a16-4e04-9ece-e0265cfba3c8";
 export const anonPictureUrl = "https://firebasestorage.googleapis.com/v0/b/unstuck-backend.appspot.com/o/anonymous.png?alt=media&token=19287e5e-1de0-4a40-9b68-f80e7b4b0f1a"
 
-export const dashboardUrl = "https://dashboard.beunstuck.me/"
-export const bookingUrl = "https://booking.beunstuck.me/"
-export const landingUrl = "https://beunstuck.me/"
+export const dashboardUrl = "https://dashboard.beunstuck.net/"
+export const bookingUrl = "https://booking.beunstuck.net/"
+export const landingUrl = "https://beunstuck.net/"
